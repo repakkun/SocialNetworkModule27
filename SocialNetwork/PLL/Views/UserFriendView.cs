de16d1f1f3ed687fa4db3e8c58e382ab8e07ab20
rePakkun <cj.ilya@yandex.ru>
2021-04-03 14:00:41 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/PLL/Views/UserFriendView.cs
@@ -0,0 +1,31 @@
+﻿using SocialNetwork.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class UserFriendView
+    {
+        public void Show(IEnumerable<User> friends)
+        {
+            Console.WriteLine("Мои друзья");
+
+
+            if (friends.Count() == 0)
+            {
+                Console.WriteLine("Список друзей пуст");
+                return;
+            }
+
+            friends.ToList().ForEach(friend =>
+            {
+                Console.WriteLine($"Email друга: {friend.Email}. Имя и фамилия друга: {friend.FirstName} {friend.LastName}");
+            });
+
+            }
+
+        }
+}
+
